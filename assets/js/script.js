8ae08521dc9b2718f470da24699ffd344e4a8b9a
Ricky Smithson <rickyroysmithson@gmail.com>
2024-10-15 18:10:36 +0000

    Add: code to calculate two random numbers for the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,15 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-function runGame () {
+/**
+ * The main "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 
+function runGame () {
+    // Create two random numbers 1- 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function chackAnswer() {
